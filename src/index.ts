a9822531593b6a1fcffe690e621624373e751005
naufaladrian <naufal.adrian904@gmail.com>
2024-11-09 19:21:06 +0700

    .

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ const port = process.env.PORT || 3000;
 
 app.listen(port)
 
-console.log(`🦊 Elysia is running at http://localhost:${port}`);
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
 
